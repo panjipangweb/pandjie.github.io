672a866108f870f59eaf0254f1710e91be99c95c
Bran <111070465+panjipangweb@users.noreply.github.com>
2023-09-14 10:36:34 +0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function AboutPage() {
         <Fragment>
           <Meta
             lang="id"
-            title="ℹ️ About // mazipan.space"
+            title="ℹ️ About // inipanjie.netlify.app"
             description="About the Author of mazipan.space"
             url={`${SITE_METADATA.url}/about`}
             coverImage={`${OG_IMAGE_ABOUT}`}
@@ -21,112 +21,16 @@ export default function AboutPage() {
           </h2>
           <div className="content">
             <h2 className="text-2xl md:text-4xl font-bold mb-4 tracking-tighter leading-tight">
-              👋 Hi there, I am Irfan Maulana.
+              👋 Hi there, I am Panji Ariyo.
             </h2>
             <p>
-              A seasoned web developer from Indonesia with 10+ years of experience in the e-commerce industry. Ex Sayurbox, Tokopedia, Bizzy Indonesia, Blibli.com, and SML Technologies.
+             I'm passionate about learning languages, cultures, and linguistics. I'm also an amateur writer who loves to write about anything that interests me.
             </p>
             <br />
             <p>
-              Most of the time was working with <code className="rounded-full px-1 text-sm bg-red-100 text-red-600">JavaScript</code> & <code className="rounded-full px-1 text-sm bg-red-100 text-red-600">TypeScript</code> in the Frontend side. Was using various frameworks such as <code className="rounded-full px-1 text-sm bg-red-100 text-red-600">React</code>, <code className="rounded-full px-1 text-sm bg-red-100 text-red-600">Svelte</code>, & <code className="rounded-full px-1 text-sm bg-red-100 text-red-600">Vue</code>.
+             I'm a self-taught front-end developer and technical writer. I've published two books and several research papers. I'm always looking for new ways to learn and grow.
             </p>
             <br />
-            <p>
-              Helping OSS projects in <a
-                className="text-red-500"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/phpid-jakarta">PHPID</a>, <a
-                  className="text-red-500"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://twitter.com/reactjsid">React.js ID</a>, <a
-                    className="text-red-500"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/kawalcovid19">KawalCovid19</a>, and many more.
-            </p>
-
-            <h3 className="text-2xl md:text-4xl font-bold my-4 tracking-tighter leading-tight">
-              Career
-            </h3>
-
-            <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              {CAREERS.map((career) => (
-                <li
-                  key={career.period}
-                  className="rounded-lg p-4 overflow-hidden shadow-lg border-2 border-red-500 bg-white dark:bg-gray-800"
-                >
-                  <h4 className="text-lg text-red-500 font-bold mb-2">
-                    {career.title}
-                  </h4>
-                  <div className="text-sm">
-                    <a
-                      className="underline hover:text-red-500"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href={career.link}>
-                      {career.company}
-                    </a>
-                    {` • `}
-                    <span>{career.location}</span>
-                  </div>
-                  <div className="text-gray-600 dark:text-gray-200">
-                    <small>{career.period}</small>{` • `}<small>{career.long}</small>
-                  </div>
-                </li>
-              ))}
-            </ul>
-
-            <h3 className="text-2xl md:text-4xl font-bold my-4 tracking-tighter leading-tight">
-              Featured OSS
-            </h3>
-
-
-            <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              {FEATURED_OSS.map((oss) => (
-                <li
-                  key={oss.repo}
-                  className="rounded-lg p-4 overflow-hidden shadow-lg border-2 border-red-500 bg-white dark:bg-gray-800"
-                >
-                  <a
-                    className="hover:underline text-red-500"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={oss.url}
-                  >
-                    <h4 className="text-lg font-bold mb-4">
-                      🌏 {oss.title}
-                    </h4>
-                  </a>
-                  <p className="mb-4 text-sm">{oss.desc}</p>
-
-                  <div className="flex justify-between">
-                    <a
-                      className="hover:underline text-red-500 text-sm"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href={oss.repo}
-                    >
-                      📦 {oss.repo.replace('https://github.com/', '')}
-                    </a>
-                    <img src={`https://img.shields.io/github/stars/${oss.repo.replace('https://github.com/', '')}.svg?style=social`} alt={oss.repo} loading="lazy" />
-                  </div>
-
-                  <div className="flex mt-4">
-                    {oss.stacks.map((stack) => (
-                      <span
-                        key={stack}
-                        className="inline-block bg-red-100 text-red-600 rounded-full px-2 py-1 text-xs font-semibold mr-2"
-                      >
-                        {stack}
-                      </span>
-                    ))}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
         </Fragment>
       </LayoutArticle>
     </>
